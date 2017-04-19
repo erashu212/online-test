0773cbc37132635be5100e5a7f1937831bd312eb
Kibeom Kim <kk1674@nyu.edu>
2017-04-19 12:16:56 -0700

    Add HammerJS to fix Angular Material error (#42)
    Fixes the following client side error by adding HammerJS
    
    "Could not find HammerJS. Certain Angular Material components may not
    work correctly.  GestureConfig @ vendor.bundle.js:38735"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 import * as io from 'socket.io-client';
 import { Converter } from 'showdown/dist/showdown';
+import 'hammerjs'
 
 @Component({
   selector: 'app-root',
